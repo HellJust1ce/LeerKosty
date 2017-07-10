40c96904f93dbf04505b3a85f6be34b4e21abe75
HellJust1ce <kiselev91kv@gmail.com>
2017-07-10 20:58:55 +0300

    First commit

---
+++ src/main/java/test/TestFor.java
@@ -0,0 +1,31 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by mj2like on 02.07.2017.
+ */
+public class TestFor {
+
+
+    private int int1;
+
+    public int int1 (){
+        return this.int1 + 1;
+    }
+    public void g (){
+        this.int1 += 1;
+        System.out.println(int1);
+    }
+    public static void main(String[] args) {
+        String[] arr = {"Gleb", "Kosty", "Max", "Serge"};
+
+       /* for (int i = 0; i <= 3; i++) {
+            System.out.print(arr[i] + " ");
+        }*/
+            TestFor testFor = new TestFor();
+            System.out.println(testFor.int1());
+        testFor.g();
+    }
+}
